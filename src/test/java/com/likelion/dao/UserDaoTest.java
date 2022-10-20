f0625a32bc667a9373a3e7a5a05bb0def847f91f
lucyoz <lucyoz.kwon@gmail.com>
2022-10-20 13:32:21 +0900

    deleteAll(), getCount() Test추가

--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -21,16 +21,36 @@ class UserDaoTest {
     @Test
     void addAndSelect() throws SQLException {
         UserDao userDao = context.getBean("awsUserDao",UserDao.class);
-        UserDao userDao2 = context.getBean("awsUserDao",UserDao.class);
-
-        System.out.println(userDao);
-        System.out.println(userDao2);
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
 
         String id="24";
         userDao.add(new User(id, "nunu","123qwe"));
+        assertEquals(1, userDao.getCount());
 
         User user = userDao.findById(id);
         assertEquals("nunu",user.getName());
         assertEquals("123qwe",user.getPassword());
     }
+
+    @Test
+    void count() throws SQLException {
+        User user1 = new User("1","kyeonghwan","1123");
+        User user2 = new User("2","sohyun","1234");
+        User user3 = new User("3","sujin","4321");
+
+
+        UserDao userDao = context.getBean("awsUserDao",UserDao.class);
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+        userDao.add(user2);
+        assertEquals(2, userDao.getCount());
+        userDao.add(user3);
+        assertEquals(3, userDao.getCount());
+    }
+
+
 }
